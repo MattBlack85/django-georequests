bdc9df4c392fe0b340d09cafeea46410f14a9a40
MattBlack85 <promat85@gmail.com>
2014-04-13 22:09:58 +0200

    Removed django project to make room for django reusable app.

--- user_data/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-import jsonfield
-
-
-class users(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    location = jsonfield.JSONField()
