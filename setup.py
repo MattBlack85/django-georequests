35690a28e4caf5e6bf89120771e343e5bca93689
MattBlack85 <promat85@gmail.com>
2014-09-28 17:41:36 +0200

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-georequests',
-    version='0.2.0-alpha',
+    version='0.2.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=reqs,
