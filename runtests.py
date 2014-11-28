a4c09c2010d61da12f41037ef0b2c9bdb069deeb
MattBlack85 <promat85@gmail.com>
2014-11-28 21:36:25 +0100

    Tune test settings

--- runtests.py
+++ runtests.py
@@ -8,12 +8,12 @@ def run_tests():
     from django.conf import global_settings
     from django.conf import settings
 
-
     settings.configure(
         INSTALLED_APPS=[
-            'geo_middleware',
-            'reversegeoip',
-            'geodashboard'
+            'djeorequests',
+            'djeorequests.geo_middleware',
+            'djeorequests.geodashboard',
+            'djeorequests.reversegeoip'
         ],
         DATABASES={
             'default': {
@@ -29,8 +29,9 @@ def run_tests():
 
     test_runner = DjangoTestSuiteRunner(verbosity=2)
     return test_runner.run_tests(['geo_middleware',
-                                  'reversegeoip',
-                                  'geodashboard'])
+                                  'geodashboard',
+                                  'reversegeoip'])
+
 
 def main():
     failures = run_tests()
